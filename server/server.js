b85002ac58cfe81127e78ca7b85c69e66d656eea
vspits <spits.vivian@gmail.com>
2019-04-19 13:43:45 -0600

    some tests are working, some are not. working on req.queries.

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get(`/api/user/:userid`, ctrl.getUserId)
 app.get(`/api/admin`, ctrl.getAdmins)
 app.get(`/api/nonadmin`, ctrl.getNonadmins)
 app.get(`/api/type/:user`, ctrl.getUserType)
-app.put(`/api/user/:userId`, ctrl.updateUser)
+app.put(`/api/user/:id`, ctrl.updateUser)
 app.post(`/api/user`, ctrl.addUser)
 app.delete(`/api/user/:userId`, ctrl.deleteUser)
 
--- server/usersCtrl.js
+++ server/usersCtrl.js
@@ -1,12 +1,32 @@
-let userData = require('./../userData.json')
+const userData = require('./../userData.json')
 
 module.exports = {
     getAllUsers: (req, res, next) => {
+        const { age, email, favorites } = req.query
+
+        const queryMap = userData.map(user => {
+            if(user.age < age){
+                console.log(user)
+                res.send(user)
+            } 
+    
+            if(user.email === email){
+                res.send(user)
+            }
+    
+            if(user.favorites.includes(favorites)){
+                res.send(user)
+            }
+        })
+
         res.status(200).send(userData)
+
     },
     getUserId: (req, res, next) => {
+        // find the user id and convert to a number with +
         const findUserId = userData.findIndex(user => user.id === +req.params.userid)
         
+        // if there was an id found, it will send. If not, return as not found.
         if(userData[findUserId]){
             res.status(200).send(userData[findUserId])
         }
@@ -30,16 +50,63 @@ module.exports = {
         res.status(200).send(userData[userType])
     },
     updateUser: (req, res, next) => {
-        
+        const { id } = req.params
+        const { first_name, last_name, email, gender, language, age, city, state, type, favorites } = req.body
+
+        const updatedUsers = userData.map(user => {
+            // console.log(id)
+            if(user.id === id){
+                user.first_name.push(first_name)
+                user.last_name.push(last_name)
+                user.email.push(email)
+                user.gender.push(gender)
+                user.language.push(language)
+                user.age.push(age)
+                user.city.push(city)
+                user.state.push(state)
+                user.type.push(type)
+                user.favorites.push(favorites)
+            }
+        })
+
+        // console.log(updatedUsers)
+        res.status(200).send(updatedUsers)
     },
     addUser: (req, res, next) => {
-        req.body.id = id
-        id++
-        userData.push(req.body)
+
+        //figure out the length of the current array and make the id equal to that plus one
+        let id = userData[userData.length - 1].id + 1
+
+        //make the new user object
+        const newUser = {
+            id: id,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email, 
+            gender: req.body.gender, 
+            language: req.body.language, 
+            age: req.body.age, 
+            city: req.body.city, 
+            state: req.body.state, 
+            type: req.body.type, 
+            favorites: req.body.favorites
+        }
+
+        //add new user to the end of the array
+        userData.push(newUser)
         res.sendStatus(201)
     },
     deleteUser: (req, res, next) => {
-        userData = userData.filter(user => user.id !== +req.params.id)
+        //get the user id to delete off of params
+        const userToDelete = req.params.id*1
+
+        //find the index of the user to delete based on id
+        let userIndex = userData.findIndex(user => user.index === userToDelete)
+
+        //remove the users index with splice
+        userData.splice(userIndex, 1)
+
+        //send back userData after spliced
         res.status(200).send(userData)
     }
 }
